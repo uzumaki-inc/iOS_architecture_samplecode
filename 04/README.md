53eb59a6e077f1f9aaf44db59c8767d13950d09f
satoutakeshi <sato.takeshi1987@gmail.com>
2019-02-17 21:14:07 +0900

    add read me

---
+++ 04/README.md
@@ -0,0 +1,23 @@
+# Fluxサンプル
+
+![screen shot]()
+
+Flux構成でボタンをタップすると背景色が変化するサンプルアプリを作成しました。
+
+# 概要
+
+* Change Colorで背景色をランダムで変更
+* ランダムの結果、現在表示している色と同じだったらもう一度やり直し、必ず他の色が表示されるようにする
+
+# Flux構成
+
+![Flux]()
+
+
+
+
+# 参考
+
+[iOSアプリ設計パターン入門](https://peaks.cc/books/iOS_architecture)
+
+[10分で実装するFlux](http://azu.github.io/slide/react-meetup/flux.html)
--- 04/TryFlux/TryFlux/Flux/ActionCreator.swift
+++ 04/TryFlux/TryFlux/Flux/ActionCreator.swift
@@ -18,12 +18,13 @@ final class ActionCreator {
 
 // MARK: change color
 extension ActionCreator {
-    //　処理を書く所？
+    //　処理を書く所
     func changeColor(currentColor: UIColor) {
         let newColor = makeNewColor(current: currentColor)
         dispatcher.dispatch(.changeColor(newColor))
     }
 
+    // 現在のカラーと異なるFlatPaletteを返す。ランダムの結果、現在と同じColorだったらもう一度やり直す。
     private func makeNewColor(current: UIColor) -> FlatPalette {
         let ramdomValue = Int.random(in: 0 ..< FlatPalette.allCases.count)
         guard let ramdomPalette = FlatPalette(rawValue: ramdomValue) else {
